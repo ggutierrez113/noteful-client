c08b8c7e404b8061c4c7e8189e4355512901bac4
Gerardo Gutierrez <jerrysmacbook@air.lan>
2020-04-29 15:21:29 -0500

    redo

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-  API_ENDPOINT: `https://tranquil-sands-83321.herokuapp.com/`,
+  API_ENDPOINT: `https://obscure-forest-99041.herokuapp.com/`,
+  API_KEY: process.env.REACT_APP_API_KEY,
 };
